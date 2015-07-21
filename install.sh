72fbb48e8e1b713f162a5c42548152892ae28c7e
Philip Trauner <philip.trauner@aol.com>
2015-07-21 09:46:06 +0200

    Made sure sudo is installed

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "Python 2.7 required for installation. Downloading..."
+echo "Python 2.7 required for installation. Installing..."
 sudo apt-get install -y python
+echo "Installing sudo."
+sudo apt-get install -y sudo
 python install.py
