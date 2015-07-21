bf041d64eac2070a4357fb3f95c347925e263484
Philip Trauner <philip.trauner@aol.com>
2015-07-21 14:34:41 +0200

    Automatically add all files in folder to __all__

--- Modules/Drivers/__init__.py
+++ Modules/Drivers/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ["SR830", "GenericDriver"]
+import os
+for module in os.listdir(os.path.dirname(__file__)):
+    if module == '__init__.py' or module[-3:] != '.py':
+        continue
+    __import__(module[:-3], locals(), globals())
+del module
+del os
