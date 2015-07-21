feca2f3ade14dd7287c5d302969decade0584c35
Philip Trauner <philip.trauner@aol.com>
2015-07-21 13:49:42 +0000

    Changed stuff

--- Modules/sr830_demo.py
+++ Modules/sr830_demo.py
@@ -4,6 +4,7 @@ g = GPIB(debug=True)
 print("Devices: " + str(g.devices))
 if len(g.devices) > 0:
 	port = int(raw_input("Port: "))
+	print(g.devices[port])
 	i = 1
 	while 1:
 		print("%s: %s" % (str(i), g.devices[port].getR()))
