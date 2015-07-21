ef3a90b4a9da303f5e492df10699ddc4a15d421a
Philip Trauner <philip.trauner@aol.com>
2015-07-21 11:10:22 +0200

    Check if devices found added

--- Modules/sr830_demo.py
+++ Modules/sr830_demo.py
@@ -2,8 +2,9 @@ from GPIB import GPIB
 
 g = GPIB()
 print("Devices: " + str(g.devices))
-port = int(raw_input("Port: "))
-i = 1
-while 1:
-	print("%s: %s" % (str(i), g.devices[2].getR()))
-	i += 1
+if len(g.devices) > 0:
+	port = int(raw_input("Port: "))
+	i = 1
+	while 1:
+		print("%s: %s" % (str(i), g.devices[port].getR()))
+		i += 1
