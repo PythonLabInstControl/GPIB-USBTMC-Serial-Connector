9f4952a20d97a61b391ca05f903af3b3d740810d
Philip Trauner <philip.trauner@aol.com>
2015-07-21 13:51:49 +0000

    Removed debug print

--- Modules/sr830_demo.py
+++ Modules/sr830_demo.py
@@ -4,7 +4,6 @@ g = GPIB(debug=True)
 print("Devices: " + str(g.devices))
 if len(g.devices) > 0:
 	port = int(raw_input("Port: "))
-	print(g.devices[port])
 	i = 1
 	while 1:
 		print("%s: %s" % (str(i), g.devices[port].getR()))
