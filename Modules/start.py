9503fc5ba4a5a88ddc936581d9ebceb32c8a9dea
Philip Trauner <philip.trauner@aol.com>
2015-07-22 08:19:11 +0000

    Fixed import issue related to relative paths

--- Modules/start.py
+++ Modules/start.py
@@ -12,9 +12,13 @@ if sys.platform == "win32":
 if len(sys.argv) == 2:
 	if os.path.isfile(sys.argv[1]):
 		import_path = os.path.dirname(sys.argv[1])
-		sys.path.append(import_path)
-		os.chdir(import_path)
+		if import_path != "":
+			sys.path.append(os.path.abspath(import_path))
+			print(sys.path)
+			os.chdir(import_path)
 		importlib.import_module(os.path.basename(os.path.splitext(sys.argv[1])[0]))
+	else:
+		Logging.error("File does not exist")
 else:
 	Logging.error("Specify python file as second argument")
 
