c4c1c2902b11f25a9ded8a8bce95e8f25ed2746d
Philip Trauner <philip.trauner@aol.com>
2015-07-23 13:38:12 +0000

    Now prints out beeper state

--- Modules/Demos/33220a_demo.py
+++ Modules/Demos/33220a_demo.py
@@ -2,5 +2,6 @@ from USBTMC import USBTMC
 
 usb = USBTMC(debug=True)
 for i in usb.devices:
-	if "33220a" in i.lower():
+	if "33220a" in usb.devices[i].device_id.lower():
 		print("Beeper state: %s" % ("ON" if usb.devices[i].get_beeper_state() == "1" else "OFF"))
+				
