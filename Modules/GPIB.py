d19a4aebf77c0b654060b8bf876998816f5fd55d
Philip Trauner <philip.trauner@aol.com>
2015-07-27 07:13:06 +0000

    Fixed indentation

--- Modules/GPIB.py
+++ Modules/GPIB.py
@@ -88,12 +88,12 @@ class GPIB:
 				os.system('echo -n %s | sudo tee /sys/bus/pci/drivers/ehci-pci/unbind' % i)
 				os.system('echo -n %s | sudo tee /sys/bus/pci/drivers/ehci-pci/bind' % i)
 
-		def reset_debian7(self):
-				ehci_content = os.listdir("/sys/bus/pci/drivers/ehci_hcd/")
-				for i in ehci_content:
-						if i[0] == "0":
-								os.system('echo -n %s | sudo tee /sys/bus/pci/drivers/ehci_hcd/unbind' % i)
-								os.system('echo -n %s | sudo tee /sys/bus/pci/drivers/ehci_hcd/bind' % i)
+	def reset_debian7(self):
+		ehci_content = os.listdir("/sys/bus/pci/drivers/ehci_hcd/")
+		for i in ehci_content:
+			if i[0] == "0":
+				os.system('echo -n %s | sudo tee /sys/bus/pci/drivers/ehci_hcd/unbind' % i)
+				os.system('echo -n %s | sudo tee /sys/bus/pci/drivers/ehci_hcd/bind' % i)
 
 	def reset_usb(self):
 		if self.debug: Logging.info("Resetting connected usb interfaces")
