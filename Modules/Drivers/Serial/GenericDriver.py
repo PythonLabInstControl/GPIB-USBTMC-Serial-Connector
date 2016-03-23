fb2b658b531c2ecfdb4cefd17614b55fa4a93807
HoWilgh <HoWilgh@users.noreply.github.com>
2016-03-23 16:55:25 +0100

    updated for pyserial 3.X and added TextIOWrapper
    updated for pyserial 3.X and added TextIOWrapper
    Added also some bugfixing for SR830 giving '' return strings

--- Modules/Drivers/Serial/GenericDriver.py
+++ Modules/Drivers/Serial/GenericDriver.py
@@ -1,21 +1,34 @@
 import time
 
 class GenericDriver:
-	def __init__(self, communicator, device_id, debug=False):
-		self.communicator = communicator
-		self.device_id = device_id
-		self.debug = debug
+    def __init__(self, communicator, device_id, debug=False):
+        self.communicator = communicator
+        self.device_id = device_id
+        self.debug = debug
 
 
-	def get(self, cmd):
-		self.write(cmd)
-		return self.communicator.readline()
+    def get(self, cmd):
+        self.communicator.flush()
+        self.communicator.readline()
+        
+        self.write(cmd)
+        ret = self.communicator.readline()
+        ret = ret.rstrip()
+        
+        # Doing some bugfixing for the SR830 which gives sometimes ''
+        #print('     ret = '+ret)
+        #print(type(ret))
+        if (ret == ''): print('Error "" ###### :'+ret); ret = self.get(cmd)
+        elif (ret == ' '): print('Error " " ###### :'+ret); ret = self.get(cmd)
+        elif (ret == '\n'): print('Error "\\n" ###### :'+ret); ret = self.get(cmd)
+        elif (ret == '\r'): print('Error "\\r" ###### :'+ret); ret = self.get(cmd)
+        return ret
 
 
-	def set(self, cmd, param):
-		self.write(cmd + " " + str(param))
+    def set(self, cmd, param):
+        self.write(cmd + " " + str(param))
 
 
-	def write(self, cmd):
-		self.communicator.write(cmd + "\r\n")
-		time.sleep(0.1)
+    def write(self, cmd):
+        self.communicator.write(unicode(cmd+"\n"))        
+        time.sleep(0.05)
